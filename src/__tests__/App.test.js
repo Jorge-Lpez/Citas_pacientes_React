deb122c77de06b449c6ee663ec6fb79242091000
jorge abascal lopez <jorgeabascal20@gmail.com>
2020-11-30 00:26:03 -0500

    Agregando carpeta de tests en react

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,7 @@
+// npm run test   // Para ejecutar el test en react js 
+// Se puede mantener el archivo App.test.js en el directorio SRC o se puede crear la carpeta
+// __tests__ y dentro de esta agregar los archivos de test
+
+test("La aplicacion funciona correctamente", () => {
+
+});
